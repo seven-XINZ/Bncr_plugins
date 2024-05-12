8d99729ed83c691089a9819b576c2cd801d9815c
victorli-cn <91169060+victorli-cn@users.noreply.github.com>
2024-05-12 22:37:58 +0800

    Update 万能翻译.js

--- 万能翻译.js
+++ 万能翻译.js
@@ -5,7 +5,11 @@
  * @version 1.0.0
  * @description 博天api支持28种语言翻译
  * @rule ^(fy|翻译)([^\n]+)$
+ * @rule ^(动漫)([^\n]+)$
+ * @rule ^(真人)([^\n]+)$
+ * @rule ^(卡通)([^\n]+)$
  * @admin false
+ * @priority 999
  * @disable false
  */
 
@@ -14,14 +18,23 @@ module.exports = async s => {
 	const axios = require("axios").default
 	let par = encodeURI(s.param(2))
 	let res = await axios.request({
-		url: "http://www.plapi.tk/api/fanyi.php?msg=" + par,
+		url: "https://api.oick.cn/api/fanyi?text=" + par,
 		method: "GET",
 		json: true
 	})
 	if(res.status == 200) {
 		console.log(res)
-		let data = res.data.replace('有道翻译查询结果', '')
-		await s.reply(data)
+		let data = res.data.data.result
+		if(s.param(1) == "动漫") {
+			await s.inlineSugar(`rr ${data}`)
+		} else if(s.param(1) == "真人") {
+			await s.inlineSugar(`ps ${data}`)
+		} else if(s.param(1) == "卡通") {
+			await s.inlineSugar(`ee ${data}`)
+		} else {
+			await s.reply(data)
+		}
+		return 'next'
 	}
 	else {
 		return await s.reply("接口异常~")
